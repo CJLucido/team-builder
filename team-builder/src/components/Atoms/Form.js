496c0ce7772c337abaea4e3aeb6f564eb578fae6
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-16 12:23:28 -0700

    added label and input for name in Form

--- team-builder/src/components/Atoms/Form.js
+++ team-builder/src/components/Atoms/Form.js
@@ -4,8 +4,13 @@ import React from "react";
 function Form(){
 
     return (
-
+    <div>
         <h1>Form</h1>
+
+        <label htmlFor="name">Name:</label>
+        <input id="name" name="name" placeholder="Type your name here" type="text"/>
+
+    </div>
     )
 }
 
