f4024a659b594c5308c227ea95a92a5ee481404b
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-16 12:17:56 -0700

    created Form in atom components folder, imported Form to App, using placeholder text to wire up imports

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Atoms/Form"
 
 function App() {
    const [teamList, setTeamList] = useState([]);
   return (
     <div className="App">
      <h1>Hello World</h1>
+     <Form/>
     </div>
   );
 }
