7ab18ee02395dde5376a6005f13a3ea58dbd50e6
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-16 12:39:56 -0700

    wrapped form in form tag with an onSubmit (handler not yet made)

--- team-builder/src/components/Atoms/Form.js
+++ team-builder/src/components/Atoms/Form.js
@@ -13,16 +13,16 @@ function Form(){
     return (
     <div>
         <h1>Form</h1>
+        <form onSubmit={submitForm}>
+            <label htmlFor="name">Name: </label>
+            <input id="name" name="name" placeholder="Type your name here" type="text"/>
 
-        <label htmlFor="name">Name: </label>
-        <input id="name" name="name" placeholder="Type your name here" type="text"/>
+            <label htmlFor="email">Email: </label>
+            <input id="email" name="email" placeholder="Type your email here" type="text"/>
 
-        <label htmlFor="email">Email: </label>
-        <input id="email" name="email" placeholder="Type your email here" type="text"/>
-
-        <label htmlFor="role">Position: </label>
-        <input id="role" name="role" placeholder="Type your role here" type="text"/>
-
+            <label htmlFor="role">Position: </label>
+            <input id="role" name="role" placeholder="Type your role here" type="text"/>
+        </form>
     </div>
     )
 }
