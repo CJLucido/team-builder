8201494a23ba3b42d8f73e4075b60f81e665a4af
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-16 12:26:25 -0700

    added inputs and labels for emails and roles to Form

--- team-builder/src/components/Atoms/Form.js
+++ team-builder/src/components/Atoms/Form.js
@@ -7,9 +7,15 @@ function Form(){
     <div>
         <h1>Form</h1>
 
-        <label htmlFor="name">Name:</label>
+        <label htmlFor="name">Name: </label>
         <input id="name" name="name" placeholder="Type your name here" type="text"/>
 
+        <label htmlFor="email">Email: </label>
+        <input id="email" name="email" placeholder="Type your email here" type="text"/>
+
+        <label htmlFor="role">Position: </label>
+        <input id="role" name="role" placeholder="Type your role here" type="text"/>
+        
     </div>
     )
 }
